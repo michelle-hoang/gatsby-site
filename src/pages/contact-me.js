38ee6a0a8a594886b6f0438c8fb4cc28c2989fde
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-09-11 09:39:32 -0500

    change to mobile

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Resume from "../components/resume"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,7 +8,30 @@ const ContactMe = () => (
   <Layout>
     <SEO title="Contact Me" />
     <h2>Contact Me</h2>
-    <div className="container"></div>
+    <div className="container">
+      <div>
+        <p>
+          <span className="highlight">hello@michellehoang.dev</span>
+        </p>{" "}
+        <p>
+          <span className="highlight">
+            <a href="https://www.linkedin.com/in/hoangmichelle">
+              linkedin.com/in/hoangmichelle/
+            </a>
+          </span>
+        </p>
+        <p>
+          <span className="highlight">
+            <a href="https://www.github.com/michelle-hoang">
+              github.com/michelle-hoang
+            </a>
+          </span>
+        </p>
+        <p>Download my resume here</p>
+      </div>
+
+      <div></div>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
