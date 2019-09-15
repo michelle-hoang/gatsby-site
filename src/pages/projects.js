f943ea36da9952a43a235e70f21fc3942cd5bbb4
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-09-15 12:46:03 -0500

    updated image in projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Stroll = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "stroll.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 800, quality: 95) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+// const Breadshop = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       placeholderImage: file(relativePath: { eq: "breadshop.png" }) {
+//         childImageSharp {
+//           fluid(maxWidth: 800, quality: 95) {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+// }
 
 const Projects = () => (
   <Layout>
@@ -9,6 +43,10 @@ const Projects = () => (
       <h2 style={{ textAlign: "center" }}>Projects</h2>
       <div>
         <h4>Stroll</h4>
+        <div style={{ alignItems: `center`, width: `40rem` }}>
+          <Stroll />
+        </div>
+
         <p>
           A mobile app where users can view popular attractions and find or
           create walks in their area <br />
@@ -18,6 +56,7 @@ const Projects = () => (
       </div>
       <div>
         <h4>From D3 to Victory</h4>
+        <div style={{ alignItems: `center`, width: `40rem` }}></div>
         <p>
           A web app using data visualizations about my experience learning D3
           and Victory.js during a 4-day hackathon <br />
@@ -26,6 +65,7 @@ const Projects = () => (
       </div>
       <div>
         <h4>Let's get this bread (shop)</h4>
+        <div style={{ alignItems: `center`, width: `40rem` }}></div>
         <p>
           An e-commerce website selling bread and related products <br />
           Technologies implemented: Express, Sequelize, React, Redux,
